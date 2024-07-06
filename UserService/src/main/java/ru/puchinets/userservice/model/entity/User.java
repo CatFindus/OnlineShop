033722cd7895667ba097fe54250e3af1492b4f6a
Anton Puchinets <apuchinec@gmail.com>
2024-07-06 13:49:30 +0300

    Beginning project OnlineShop

---
+++ UserService/src/main/java/ru/puchinets/userservice/model/entity/User.java
@@ -0,0 +1,35 @@
+package ru.puchinets.userservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+    @Column(name = "firstname")
+    private String firstname;
+    @Column(name = "lastname")
+    private String lastName;
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
